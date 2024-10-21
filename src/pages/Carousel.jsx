d1580c7de8d199d61c19f981bc40fd1d533412e3
Yasir Rabbani Tanvir <yasirrabbanitanvir@gmail.com>
2024-10-22 00:48:30 +0600

    update

--- src/pages/Carousel.jsx
+++ src/pages/Carousel.jsx
@@ -38,50 +38,22 @@ class Carousel extends Component {
       <div>
         <div id="carouselExampleFade" className="carousel slide carousel-fade container mt-5" data-bs-ride="carousel">
           <div className="carousel-inner">
-            <div className="carousel-item active">
-              <div className='row'>
-                <div className='col-md-6'>
-                  <img src="/img/ieee 1 (1).jpg" className="d-block w-100" alt="..." /> {/* Adjusted path */}
-                </div>
-                <div className='col-md-6'>
-                  <p className='shortTitle'>{this.state.ShortTitle.Stitle1}</p>
-                  <h3 className='longTitle'>{this.state.LongTitle.Ltitle1}</h3>
-                  <p className='shortTitle'>{this.state.Paragraph.Ptitle1}</p>
-                  <p className='channelName'>{this.state.ChannelName.ChannelName1}</p>
-                  <p className='shortTitle'>{this.state.Date.date1}</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="carousel-item">
-              <div className='row'>
-                <div className='col-md-6'>
-                  <img src="/img/ieee 1 (2).jpg" className="d-block w-100" alt="..." /> {/* Adjusted path */}
-                </div>
-                <div className='col-md-6'>
-                  <p className='shortTitle'>{this.state.ShortTitle.Stitle2}</p>
-                  <h3 className='longTitle'>{this.state.LongTitle.Ltitle2}</h3>
-                  <p className='shortTitle'>{this.state.Paragraph.Ptitle2}</p>
-                  <p className='channelName'>{this.state.ChannelName.ChannelName2}</p>
-                  <p className='shortTitle'>{this.state.Date.date2}</p>
+            {Object.keys(this.state.ShortTitle).map((key, index) => (
+              <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
+                <div className='row'>
+                  <div className='col-md-6 col-12'>
+                    <img src={`/img/ieee 1 (${index + 1}).jpg`} className="d-block w-100" alt={`Slide ${index + 1}`} />
+                  </div>
+                  <div className='col-md-6 col-12 d-flex flex-column justify-content-center'>
+                    <p className='shortTitle'>{this.state.ShortTitle[key]}</p>
+                    <h3 className='longTitle'>{this.state.LongTitle[`Ltitle${index + 1}`]}</h3>
+                    <p className='shortTitle'>{this.state.Paragraph[`Ptitle${index + 1}`]}</p>
+                    <p className='channelName'>{this.state.ChannelName[`ChannelName${index + 1}`]}</p>
+                    <p className='shortTitle'>{this.state.Date[`date${index + 1}`]}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-
-            <div className="carousel-item">
-              <div className='row'>
-                <div className='col-md-6'>
-                  <img src="/img/ieee 1 (3).jpg" className="d-block w-100" alt="..." /> {/* Adjusted path */}
-                </div>
-                <div className='col-md-6'>
-                  <p className='shortTitle'>{this.state.ShortTitle.Stitle3}</p>
-                  <h3 className='longTitle'>{this.state.LongTitle.Ltitle3}</h3>
-                  <p className='shortTitle'>{this.state.Paragraph.Ptitle3}</p>
-                  <p className='channelName'>{this.state.ChannelName.ChannelName3}</p>
-                  <p className='shortTitle'>{this.state.Date.date3}</p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
             <span className="carousel-control-prev-icon"></span>
